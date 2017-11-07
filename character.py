e9a6f76587f79a24cd6eded36725da48692db4c5
Wade Pentz <wpentz@cardinalpeak.com>
2017-11-07 12:00:17 -0700

    Some comments added to character.py

--- character.py
+++ character.py
@@ -1,1 +1,13 @@
-# This is where the general character classes will be defined.
+"""
+CHARACTERS
+
+This is where the basic character classes and their helpers are defined.
+This includes:
+- Character - Most basic and general character class
+- Protagonist - The player's character
+- Antagonist - Enemy characters
+"""
+
+# Most basic and general character class.
+class Character():
+    def __init__(self, body_attack, body_defense, mind_attack, mind_defense):
