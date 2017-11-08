417b219fa409f06b4a3a4aed2789cac5dc507411
Sam Nilsson <sam.nilsson@gmail.com>
2017-11-08 15:18:49 -0700

    Plot.py outline with logic methods

---
+++ plot.py
@@ -0,0 +1,103 @@
+
+
+
+class Condition:
+    
+    @classmethod
+    def die():
+        pass
+
+    @classmethod
+    def is_in():
+        pass
+
+
+    @classmethod
+    def less_than():
+        def f(a,b):
+            if a<b:
+                return True
+            else
+                return False
+        return f
+        pass
+
+    @classmethod
+    def greater_than(cls, a, b):
+          def f(a,b):
+            if a>b:
+                return True
+            else
+                return False
+        return f
+        pass
+    @classmethod
+    def equal_to():
+        def f(a,b):
+            if a==b:
+                return True
+            else
+                return False
+        return f
+        pass
+    @classmethod      
+    def less_than_or_equal_to():
+        pass
+        def f(a,b):
+            if a<=b:
+                return True
+            else
+                return False
+        return f
+    @classmethod      
+    def greater_than_or_equal_to():
+        pass
+        def f(a,b):
+            if a>=b:
+                return True
+            else
+                return False
+        return f
+
+
+class Plot():
+    win_condition  = []
+    lose_condition = []
+    trigger_events = []
+    
+
+    def add_win_condition():
+        pass
+    
+    def add_lose_condition():
+        pass
+
+    def check_win_condition():
+        pass
+
+    def check_lose_condition():
+        pass
+
+    def check_trigger_event():
+        pass
+
+    def cut_scene():
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
